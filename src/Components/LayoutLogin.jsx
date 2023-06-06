4e909cd6955d92472505008f458a1c999e81cc62
natalia <nespinozabar@gmail.com>
2023-06-06 16:55:27 -0500

    add colors to style

---
+++ src/Components/LayoutLogin.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+import imgRegister from "../../public/img/register.jpg";
+import logo from "../../public/img/logoseidor.png";
+import { useContext } from "react";
+import { DataContext } from "@/Context/DataContext";
+import Link from "next/link";
+
+export default function LayoutLogin({ children }) {
+  const { t } = useContext(DataContext);
+
+  return (
+    <section className="layoutLogin">
+      <div className="layoutLogin_image">
+        {/* <Image src={imgRegister} width={600} alt="imgRegister"></Image> */}
+      </div>
+      <section>
+        <nav>
+          <Image src={logo} width={120} alt="imgRegister"></Image>
+          <ul>
+            <li>
+              <Link href="/register">
+                {t.signup["Have an account?"]}
+              </Link>
+            </li>
+            <li>
+              <Link href="/register">
+                {t.signup["Log in"]}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="register">
+          register
+          {children}
+        </div>
+      </section>
+    </section>
+  );
+}
