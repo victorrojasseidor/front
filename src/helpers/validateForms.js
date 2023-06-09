cd53b7a7b44fc492eb86e9c0bac5698875560c50
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2023-06-09 02:57:25 -0500

    add icon eyes to password

--- src/helpers/validateForms.js
+++ src/helpers/validateForms.js
@@ -1,38 +1,41 @@
-import * as Yup from "yup";
+import * as Yup from 'yup'
 
 export const SignupSchemaEN = Yup.object().shape({
-  name: Yup.string().required("El nombre es requerido"),
+  name: Yup.string()
+    .matches(/^[A-Za-záéíóúÁÉÍÓÚüÜñÑ]{2,}( [A-Za-záéíóúÁÉÍÓÚüÜñÑ]{2,})?$/, 'Your name must not contain numbers or other special characters,allow up to 2 names')
+    .required('El nombre es requerido'),
   phoneNumber: Yup.string()
-    .matches(/^\d{10}$/, "Phone number must be 10 digits")
-    .required("Phone number is required"),
+    .matches(/^\d{10}$/, 'Phone number must be 10 digits')
+    .required('Phone number is required'),
   password: Yup.string()
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, "The password must have more than 8 characters between uppercase, lowercase and numbers")
-    .required("The password is required"),
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, 'The password must have more than 8 characters between uppercase, lowercase and numbers')
+    .required('The password is required'),
   confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Passwords must match")
-    .required("Password confirmation is required"),
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Password confirmation is required'),
   corporateEmail: Yup.string()
-    .email("Invalid corporate email")
-    .matches(/^(?!.*@(?:hotmail\.com|gmail\.com|yahoo\.com |outlook\.com)$)([\w.%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})$/, "The email must be from the company")
-    .required("Corporate email is required"),
-  acceptTerms: Yup.boolean().oneOf([true], "You must accept the terms and conditions.").required("You must accept the terms and conditions.")
-});
+    .email('Invalid corporate email')
+    .matches(/^(?!.*@(?:hotmail\.com|gmail\.com|yahoo\.com |outlook\.com)$)([\w.%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})$/, 'The email must be from the company')
+    .required('Corporate email is required'),
+  acceptTerms: Yup.boolean().oneOf([true], 'You must accept the terms and conditions.').required('You must accept the terms and conditions.')
+})
 
 export const SignupSchemaES = Yup.object().shape({
-  name: Yup.string().required("El nombre es requerido"),
+  name: Yup.string()
+    .matches(/^[A-Za-záéíóúÁÉÍÓÚüÜñÑ]{2,}( [A-Za-záéíóúÁÉÍÓÚüÜñÑ]{2,})?$/, 'Tu nombre no debe contener números u otros caracteres especiales, permite hasta 2 nombres')
+    .required('El nombre es requerido'),
   phoneNumber: Yup.string()
-    .matches(/^\d{10}$/, "El número de teléfono debe tener 10 dígitos")
-    .required("El número de teléfono es requerido"),
+    .matches(/^\d{10}$/, 'El número de teléfono debe tener 10 dígitos')
+    .required('El número de teléfono es requerido'),
   password: Yup.string()
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, "La contraseña debe tener más de 8 caracteres entre mayúsculas, minúsculas y números")
-    .required("La contraseña es requerida"),
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, 'La contraseña debe tener más de 8 caracteres entre mayúsculas, minúsculas y números')
+    .required('La contraseña es requerida'),
   confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Las contraseñas deben coincidir")
-    .required("La confirmación de contraseña es requerida"),
+    .oneOf([Yup.ref('password'), null], 'Las contraseñas deben coincidir')
+    .required('La confirmación de contraseña es requerida'),
   corporateEmail: Yup.string()
-    .email("Correo electrónico corporativo inválido")
-    .matches(/^(?!.*@(?:hotmail\.com|gmail\.com|yahoo\.com |outlook\.com)$)([\w.%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})$/, "El correo electrónico debe ser de la empresa")
-    .required("El correo electrónico corporativo es requerido"),
-  acceptTerms: Yup.boolean().oneOf([true], "Debes aceptar los términos y condiciones.").required("Debes aceptar los términos y condiciones.")
-});
-
+    .email('Correo electrónico corporativo inválido')
+    .matches(/^(?!.*@(?:hotmail\.com|gmail\.com|yahoo\.com |outlook\.com)$)([\w.%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})$/, 'El correo electrónico debe ser de la empresa')
+    .required('El correo electrónico corporativo es requerido'),
+  acceptTerms: Yup.boolean().oneOf([true], 'Debes aceptar los términos y condiciones.').required('Debes aceptar los términos y condiciones.')
+})
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,93 +1,92 @@
-import Link from "next/link";
-import LayoutLogin from "@/Components/LayoutLogin";
-import "../../styles/styles.scss";
-import { Formik, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import React, { useState, useContext } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import Image from "next/image";
-import logo from "../../public/img/logoseidor.png";
-import { SignupSchemaEN, SignupSchemaES } from "@/helpers/validateForms";
-import { DataContext } from "@/Context/DataContext";
+import Link from 'next/link'
+import LayoutLogin from '@/Components/LayoutLogin'
+import '../../styles/styles.scss'
+import { Formik, Field, ErrorMessage } from 'formik'
+import React, { useState, useContext } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+import logo from '../../public/img/logoseidor.png'
+import { SignupSchemaEN, SignupSchemaES } from '@/helpers/validateForms'
+import { DataContext } from '@/Context/DataContext'
 
-export default function Register() {
-  const { t, locale } = useContext(DataContext);
-  console.log("t", t);
+export default function Register () {
+  const { t, locale } = useContext(DataContext)
+  console.log('t', t)
 
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
   const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+    setShowPassword(!showPassword)
+  }
 
   const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
+    setShowConfirmPassword(!showConfirmPassword)
+  }
 
   const handleSubmit = (values) => {
     // Realizar acción cuando el formulario es válido
-    console.log("Formulario válido", values);
-  };
+    console.log('Formulario válido', values)
+  }
 
   return (
     <LayoutLogin>
       <nav>
-        <Image src={logo} width={120} alt='imgRegister'></Image>
+        <Image src={logo} width={120} alt='imgRegister' />
         <ul>
-          <li className='Question'>{t.signup["Have an account?"]}</li>
+          <li className='Question'>{t.signup['Have an account?']}</li>
           <li className='link'>
-            <Link href='/register'> {t.signup["Log in"]}</Link>
+            <Link href='/register'> {t.signup['Log in']}</Link>
           </li>
         </ul>
       </nav>
 
       <div className='register'>
-        <h1> {t.signup["Sign up"]}</h1>
-        <p> {t.signup["Create your account in SEIDOR BPaas"]}</p>
+        <h1> {t.signup['Sign up']}</h1>
+        <p> {t.signup['Create your account in SEIDOR BPaas']}</p>
 
         <Formik
           initialValues={{
-            email: "",
-            password: "",
-            confirmPassword: "",
-            name: "",
+            email: '',
+            password: '',
+            confirmPassword: '',
+            name: '',
             acceptTerms: false
           }}
-          validationSchema={locale == "es" ? SignupSchemaES : SignupSchemaEN}
+          validationSchema={locale === 'es' ? SignupSchemaES : SignupSchemaEN}
           onSubmit={handleSubmit}
         >
           {({ isSubmitting }) => (
             <form className='form'>
               <div>
-                <label htmlFor='name'>{t.signup["Username"]}</label>
+                <label htmlFor='name'>{t.signup.Username}</label>
                 <Field type='text' name='name' />
                 <ErrorMessage className='errorMessage' name='name' component='div' />
               </div>
               <div>
-                <label htmlFor='corporateEmail'> {t.signup["Company email"]}</label>
+                <label htmlFor='corporateEmail'> {t.signup['Company email']}</label>
                 <Field type='email' name='corporateEmail' />
                 <ErrorMessage className='errorMessage' name='corporateEmail' component='div' />
               </div>
 
               <div>
-                <label htmlFor='password'>{t.signup["Password"]}</label>
-                <span className='iconPassword1' onClick={togglePasswordVisibility}>
+                <label htmlFor='password'>{t.signup.Password}</label>
+                <span className='iconPassword' onClick={togglePasswordVisibility}>
                   <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
                 </span>
-                <Field type={showPassword ? "text" : "password"} id='password' name='password' />
-                
+                <Field type={showPassword ? 'text' : 'password'} id='password' name='password' />
+
                 <ErrorMessage className='errorMessage' name='password' component='span' />
               </div>
 
               <div>
-                <label htmlFor='confirmPassword'>{t.signup["Confirm password"]}</label>
-                <span className='iconPassword1' onClick={toggleConfirmPasswordVisibility}>
+                <label htmlFor='confirmPassword'>{t.signup['Confirm password']}</label>
+                <span className='iconPassword' onClick={toggleConfirmPasswordVisibility}>
                   <FontAwesomeIcon icon={showConfirmPassword ? faEyeSlash : faEye} />
-                </span> 
-                <Field type={showConfirmPassword ? "text" : "password"} id='confirmPassword' name='confirmPassword' />
-                
+                </span>
+                <Field type={showConfirmPassword ? 'text' : 'password'} id='confirmPassword' name='confirmPassword' />
+
                 <ErrorMessage className='errorMessage' name='confirmPassword' component='span' />
               </div>
 
@@ -95,19 +94,19 @@ export default function Register() {
                 <label>
                   <Field className='checkboxId' type='checkbox' name='acceptTerms' />
 
-                  <span>{t.signup["I accept"]}</span>
-                  <span> {t.signup["SEIDOR BPaaS Terms and Conditions and Privacy Policy"]}</span>
+                  <span>{t.signup['I accept']}</span>
+                  <span> {t.signup['SEIDOR BPaaS Terms and Conditions and Privacy Policy']}</span>
                 </label>
                 <ErrorMessage className='errorMessage' name='acceptTerms' component='span' />
               </div>
 
               <button className='buttonPrimary' type='submit' disabled={isSubmitting}>
-                {t.signup["SIGN UP NOW"]}
+                {t.signup['SIGN UP NOW']}
               </button>
             </form>
           )}
         </Formik>
       </div>
     </LayoutLogin>
-  );
+  )
 }
