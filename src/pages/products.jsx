da1176cfcc7d97293b70b45e409b9eeeb7595b82
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2023-06-28 17:48:37 -0500

    styles cards

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -4,6 +4,7 @@ import ImageSvg from "@/helpers/ImageSVG";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import LimitedParagraph from "@/helpers/limitParagraf";
+import { HiMenuAlt1 } from "react-icons/hi";
 
 export default function Products() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -11,22 +12,12 @@ export default function Products() {
   const [selectedFilter, setSelectedFilter] = useState(null);
 
   const products = [
-    // { id: 1, name: "Manzana", type: "Fruta" },
-    // { id: 2, name: "Banana", type: "Fruta" },
-    // { id: 3, name: "Naranja", type: "Fruta" },
-    // { id: 4, name: "Pera", type: "Fruta" },
-    // { id: 5, name: "Zanahoria", type: "Verdura" },
-    // { id: 6, name: "Tomate", type: "Verdura" },
-    // { id: 7, name: "Papa", type: "Verdura" },
-    // { id: 8, name: "Pollo", type: "Carne" },
-    // { id: 9, name: "Res", type: "Carne" },
-    // { id: 10, name: "Salmón", type: "Pescado" },
-    { id: 1, name: "Downlaod automated Bank Statements", status: "Configured", contry: "Perú", update: " ", enabled: " ", expires: "4 days", description: "Download the daily bank statement of any bank without token" },
-    { id: 2, name: "Currency Exchange rates automation", status: "Earring", contry: "Perú", update: " ", enabled: "3 months", expires: " ", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
-    { id: 3, name: "Pattern", status: "Configured", contry: "Perú", update: " ", enabled: " ", expires: "4 days", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
-    { id: 4, name: "Pattern IOP", status: "Not hired", contry: "Perú", update: " ", enabled: "3 months", expires: " ", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
-    { id: 5, name: "Exchange Rates Data API", status: "Not hired", contry: "Perú", update: " ", enabled: "3 months", expires: " ", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
-    { id: 6, name: "Real-Time  Weather Data API", status: "Not hired", contry: "Perú", update: " ", enabled: "3 months", expires: " ", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
+    { id: 1, name: "Downlaod automated Bank Statements", status: "Configured", contry: "Perú", time:{update: " ", enabled: " ", expires: "4 days"}, description: "Download the daily bank statement of any bank without token" },
+    { id: 2, name: "Currency Exchange rates automation", status: "Earring", contry: "Perú",time:{ update: " ", enabled: "3 months", expires: " "}, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
+    { id: 3, name: "Pattern", status: "Configured", contry: "Perú",time: { update: " ", enabled: " ", expires: "4 days"}, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
+    { id: 4, name: "Pattern IOP", status: "Not hired", contry: "Perú",time: {update: "3 months ", enabled: "", expires: " "}, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
+    { id: 5, name: "Exchange Rates Data API", status: "Not hired", contry: "Perú", time:{ update: "3 months ", enabled: "", expires: " "}, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
+    { id: 6, name: "Real-Time  Weather Data API", status: "Not hired", contry: "Perú", time: {update: "3 months ", enabled: "", expires: " "}, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus risus at tortor ullamcorper, nec consequat mauris gravida. Nunc at odio at velit convallis vulputate" },
   ];
 
   useEffect(() => {
@@ -60,6 +51,20 @@ export default function Products() {
     setSelectedFilter(null);
   };
 
+
+  const renderSelectedFilter = (status,time) => {
+    if (status === "Configured") {
+      return <span>Expires in {time.expires} ago</span>;
+    } else if (status === "Earring") {
+      return <span>Enabled {time.enabled} ago</span>;
+    } else if (status === "Not hired") {
+      return <span>Updated {time.update} ago</span>;
+    } else {
+      return <span>---</span>;
+    }
+  };
+
+
   return (
     <LayoutProducts>
       <div className="products">
@@ -84,7 +89,7 @@ export default function Products() {
             </button>
           </div>
           <div className="searchButton">
-            <input type="text" placeholder="Buscar..." value={searchQuery} onChange={handleInputChange} />
+            <input type="text" placeholder="Search..." value={searchQuery} onChange={handleInputChange} />
             <button onClick={handleSearch}>
               <ImageSvg name="Search" />
             </button>
@@ -93,40 +98,41 @@ export default function Products() {
 
         {searchResults.length > 0 ? (
           <div className="products_cards">
-              <p> {selectedFilter}</p>
-              <ul>
-                {searchResults.map((product) => (
-                  <li key={product.id} className="card" >
-                    <div >
-                    <ImageSvg name="Products" />
-                      <Link href="/product">
-                                              
-                          <h5> {product.name}</h5>
-                      </Link>
-                    </div>
+            <p> {selectedFilter}</p>
+            <ul>
+              {searchResults.map((product) => (
+                <li key={product.id} className={`card ${product.status === "configured" ? 'earring' : ''}`}>
+                  <div>
+                    <span>
+                      <ImageSvg name="Products" />
+                    </span>
+                    <Link href="/product">
+                      <h4> {product.name}</h4>
+                    </Link>
+                  </div>
+                  <div>
+                    <p>{product.status}</p>
+                    <span>
+                      <ImageSvg name="Schedule" />
+                      {renderSelectedFilter(product.status,product.time)}
+                     
+                    </span>
+                  </div>
 
-                    <div>
-                      <link rel="stylesheet" href="" />
-                    </div>
-                    
-                      <div>{product.status} <ImageSvg name="Schedule" /> <span></span></div>
-                    
-                      <div>
-                        <LimitedParagraph text={product.description} limit={80} />
-                      </div>
-                  
+                  <div>
+                    <LimitedParagraph text={product.description} limit={80} />
+                  </div>
 
-                    <div>
-                      <Link href="/dhdhhd">viwe more</Link>
-                      <Link href="/dhdhhd">
-                        <button>Free trial</button>
-                      </Link>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          
+                  <div>
+                    <Link href="/dhdhhd">viwe more</Link>
+                    <Link href="/dhdhhd">
+                      <button>Free trial</button>
+                    </Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         ) : (
           <p>No se encontraron resultados.</p>
         )}
