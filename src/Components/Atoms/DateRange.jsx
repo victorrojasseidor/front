027a3b9b6e87065fdd6e0433936c7c77d31fc57b
natalia <nespinozabar@gmail.com>
2023-09-08 16:57:48 -0500

    gelete error of compile

--- src/Components/Atoms/DateRange.jsx
+++ src/Components/Atoms/DateRange.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import dayjs, { Dayjs } from 'dayjs'
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
