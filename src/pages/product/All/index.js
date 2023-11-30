66299a294c69f5ff612a0e4080c5b6cccc9c57ae
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2023-11-30 18:02:02 -0500

    styles:cards

---
+++ src/pages/product/All/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LayoutProducts from '@/Components/LayoutProducts'
+
+function index (props) {
+  return (
+
+    <LayoutProducts menu='Product'>
+
+      index All
+
+    </LayoutProducts>
+
+  )
+}
+
+export default index
