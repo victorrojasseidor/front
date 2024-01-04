14912bcf0c7876f43a32b4e72bdd0c5ff82ce10b
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2024-01-04 17:49:23 -0500

    access origin uipath

--- next.config.js
+++ next.config.js
@@ -1,3 +1,42 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true
+//   },
+//   i18n: {
+//     locales: ['en', 'es'],
+//     defaultLocale: 'en'
+
+//   },
+//   reactStrictMode: true,
+//   async headers () {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         headers: [
+//           { key: 'Access-Control-Allow-Origin', value: 'https://ewtf9yqpwc.execute-api.us-east-2.amazonaws.com' },
+
+//           { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, DELETE' },
+//           { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }
+//         ]
+//       }
+//     ]
+//   },
+
+//   webpack: (config) => {
+//     config.resolve.alias = {
+//       ...config.resolve.alias,
+//       jquery: 'jquery'
+//     }
+//     return config
+//   }
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -9,7 +48,6 @@ const nextConfig = {
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en'
-
   },
   reactStrictMode: true,
   async headers () {
@@ -21,9 +59,30 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, DELETE' },
           { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }
         ]
+      },
+      {
+        source: '/api/uipath-jobs/:path*', // Ajusta la ruta según tus necesidades
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://cloud.uipath.com/demo_rch/DefaultTenant/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs' },
+          // Agrega otros encabezados según sea necesario
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, DELETE' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }]
       }
     ]
   },
+
+  // async headers () {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Origin', value: '*' },
+  //         { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, DELETE' },
+  //         { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }
+  //       ]
+  //     }
+  //   ]
+  // },
 
   webpack: (config) => {
     config.resolve.alias = {
