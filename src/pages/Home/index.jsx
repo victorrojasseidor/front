306098b7caf9453f866f3b1e569efef2ca5c25ef
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2024-02-06 18:15:20 -0500

    home: client styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import user1 from '../../../public/img/testimonials/user1.png'
 import user2 from '../../../public/img/testimonials/user2.png'
 import user3 from '../../../public/img/testimonials/user3.png'
 import user4 from '../../../public/img/testimonials/user4.png'
 import user5 from '../../../public/img/testimonials/user5.png'
+import Link from 'next/link'
+import Lang from '@/Components/Atoms/Lang'
+import logo from '../../../public/img/logoGift.gif'
 
 const Home = () => {
-// Testimonials data (testimonials.js)
+  const [selectImage, setSelectImage] = useState(null)
+  const [rotation, setRotation] = useState(0)
+
   const testimonials = [
     {
       id: 1,
@@ -47,54 +51,120 @@ const Home = () => {
     }
   ]
 
+  const changeImage = () => {
+    const nextIndex = (selectImage ? selectImage.id : 0) % testimonials.length
+    setSelectImage(testimonials[nextIndex])
+    setRotation(rotation + (360 / testimonials.length))
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(changeImage, 2000)
+
+    return () => clearInterval(intervalId)
+  }, [selectImage, rotation, testimonials])
+
   return (
     <div className='home'>
-      {/* <header>
+
+      <header>
         <nav>
+          <ul>
+
+            <div className='imgPerfil_logo'>
+
+              <Image src={logo} width='100' alt='logo' priority />
+            </div>
+          </ul>
+
           <ul>
             <li>
               <Link href='/'>
-                inciio
+                lOGIN
               </Link>
             </li>
-            <li>
+
+            <div className='languajes-box'>
+
+              <Lang />
+
+            </div>
+
+            <li className='btn_primary small'>
               <Link href='/'>
-                acera
+                SING IN
               </Link>
             </li>
 
           </ul>
         </nav>
-      </header> */}
+      </header>
 
-      <main>
+      <main className='container'>
 
-        {/* <section className='home-cara'>
-          section 1
-        </section> */}
+        <section className='home-client '>
+          <div className='subtitle'>
+            <h1>  what our client think about us? </h1>
 
-        <section className='home-client'>
-          secci+on de clientes
-
-          <div className='testimonials-container'>
-            {testimonials.map((testimonial) => (
-              <div key={testimonial.id} className='testimonial'>
-                <Image src={testimonial.image} alt={testimonial.name} width={100} height={100} />
-                <h3>{testimonial.name}</h3>
-                <p>{testimonial.position}</p>
-                <p>{testimonial.testimony}</p>
-              </div>
-            ))}
           </div>
+          <div className='testimonials-container'>
+            <div className='container-img' style={{ transform: `rotate(${rotation}deg)` }}>
+              {testimonials.map((testimonial, index) => (
+                <div
+                  key={testimonial.id}
+                  className='image-container'
+                  style={{
+                    transform: `rotate(${index * (360 / testimonials.length) + rotation}deg) translateX(150px) translateY(-50%)`,
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transformOrigin: 'center center',
+                    width: '100px',
+                    height: '100px',
+                    marginLeft: '-50px',
+                    marginTop: '-50px',
+                    transition: 'transform 1s ease'
+                  }}
+                >
+                  <button onClick={() => setSelectImage(testimonial)}>
+                    <Image
+                      className={selectImage && selectImage.id === testimonial.id ? 'active' : ''}
+                      src={testimonial.image}
+                      alt={testimonial.name}
+                      width={100}
+                      height={100}
+                    />
+                  </button>
+                </div>
+              ))}
+            </div>
 
+            {selectImage && (
+
+              <div className='testimony'>
+
+                <div className='testimony-img'>
+                  <button>
+                    <Image
+                      className='active'
+                      src={selectImage.image}
+                      alt={selectImage.name}
+                      width={100}
+                      height={100}
+                    />
+                  </button>
+                </div>
+                <div className='testimony-message'>
+                  <p>{selectImage.testimony}</p>
+                  <h3>{selectImage.name}</h3>
+                  <span>{selectImage.position}</span>
+                </div>
+
+              </div>
+            )}
+          </div>
         </section>
-
-        {/* <footer> footter </footer> */}
-
       </main>
-
     </div>
-
   )
 }
 
