8f45ceddd9621263d237d9e34aa6d4a76485cb41
NINOSKA2000 <ninoska133333@gmail.com>
2024-07-22 13:00:19 -0500

    slint warns delete

---
+++ src/pages/_documents.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Añadir fuentes aquí */}
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
