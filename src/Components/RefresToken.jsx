0a665c2fa963c13008dd5bf034f53cf68f05cd17
NINOSKA2000 <ninoska133333@gmail.com>
2024-08-22 14:01:09 -0500

    setrefreshtoken

--- src/Components/RefresToken.jsx
+++ src/Components/RefresToken.jsx
@@ -4,25 +4,30 @@ import { useAuth } from '@/Context/DataContext';
 import ImageSvg from '@/helpers/ImageSVG';
 
 function RefreshToken() {
-  const [setToken] = useState(null);
+  // const [setToken] = useState(null);
 
-  const { session, setModalToken, modalToken, l, refresToken } = useAuth();
+  const { session, setModalToken, modalToken, l, refresToken, logout } = useAuth();
   const t = l.Modal;
 
   const handleConfirmRefresh = async () => {
     try {
-      const newToken = await refresToken(session.sToken); // Llama a la función para refrescar el token
-      console.log( newToken)
+      const respToken = await refresToken(session.sToken); // Llama a la función para refrescar el token
 
-      // setToken(newToken); // Actualiza el estado con el nuevo token refrescado
-      // setModalToken(false); // Cierra el modal después de obtener el nuevo token
-      // // Refresca la página para que las demás peticiones vuelvan a funcionar con el nuevo token
-      // window.location.reload();
+      if (respToken.oAuditResponse.iCode == 1) {
+        setModalToken(false); // Cierra el modal después de obtener el nuevo token
+        window.location.reload(); // Refresca la página para que las demás peticiones vuelvan a funcionar con el nuevo token
+      } else {
+        setModalToken(true);
+      }
     } catch (error) {
       console.log('Error:', error);
       // Maneja el error si ocurre algún problema al refrescar el token
       // Puedes mostrar un mensaje de error al usuario, o tomar otras acciones apropiadas.
     }
+  };
+
+  const handleLogout = () => {
+    logout();
   };
 
   return (
@@ -38,7 +43,11 @@ function RefreshToken() {
             <div>{t['Please confirm to refresh your token and continue']}</div>
 
             <div className="box-buttons">
-              <button className="btn_primary" onClick={handleConfirmRefresh}>
+              <button className="btn_secundary" onClick={() => handleLogout()}>
+                {l.header['Sign Out']}
+              </button>
+
+              <button className="btn_primary" onClick={() => handleConfirmRefresh()}>
                 {t.Confirm}
               </button>
             </div>
