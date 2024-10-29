fba0f9c67ca3b97cf9db9d8eacfc3a593231430f
NINOSKA2000 <ninoska133333@gmail.com>
2024-10-29 15:03:41 -0500

    url dev changes

--- src/pages/reporting/reports/Detractions.jsx
+++ src/pages/reporting/reports/Detractions.jsx
@@ -5,7 +5,14 @@ import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import LoadingComponent from '@/Components/Atoms/LoadingComponent';
 import { fetchConTokenPost } from '@/helpers/fetch';
+import dayjs from 'dayjs';
 import { formatDate } from '@/helpers/report';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
 const Detracctions = () => {
   const { session, setModalToken, logout, l } = useAuth();
@@ -14,11 +21,31 @@ const Detracctions = () => {
   const [dataPadrones, setDataPadrones] = useState(null);
   const [itemsPerPage] = useState(25);
   const [page, setPage] = useState(1);
+  const [selectedCompany, setSelectedCompany] = useState('');
+  const [startDate, setStartDate] = useState(dayjs().startOf('month').format('DD/MM/YYYY'));
+  const [endDate, setEndDate] = useState(dayjs().subtract(1, 'day').format('DD/MM/YYYY'));
+  const [dataInitialSelect, setInitialDataselect] = useState([]);
 
   const t = l.Captcha;
 
   const handleChangePage = (event, value) => {
     setPage(value);
+  };
+
+  const handleStartDateChange = (newValue) => {
+    setStartDate(newValue.format('DD/MM/YYYY'));
+  };
+
+  const handleEndDateChange = (newValue) => {
+    setEndDate(newValue.format('DD/MM/YYYY'));
+  };
+
+  const handleCompanyChange = (event) => {
+    const selectCompanyValue = event.target.value;
+    setSelectedCompany(selectCompanyValue);
+   
+    
+  
   };
 
   useEffect(() => {
@@ -28,13 +55,19 @@ const Detracctions = () => {
   async function GetReportePadrones() {
     setIsLoading(true);
     const body = {
-      oResults: {},
+      oResults: {
+        FechaDesde: "06/09/2024",
+        sFechaHasta: "06/09/2024",
+        oIdEmpresa: selectedCompany ? [selectedCompany] : [],
+
+      },
+     
     };
 
     try {
       const token = session.sToken;
-      const responseData = await fetchConTokenPost('BPasS/?Accion=GetReportePadrones', body, token);
-
+      const responseData = await fetchConTokenPost('BPasS/?Accion=GetReporteDetracciones', body, token);
+       console.log({responseData});
       if (responseData.oAuditResponse?.iCode === 1) {
         const data = responseData.oResults;
         setDataPadrones(data);
@@ -70,6 +103,85 @@ const Detracctions = () => {
 
         {requestError && <div className="errorMessage"> {requestError.error}</div>}
         <div className="box-tabs">
+
+
+        <div className="box-filter">
+              <div className="date">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label={t.From}
+                    value={dayjs(startDate, 'DD/MM/YYYY')}
+                    slotProps={{
+                      textField: {
+                        // helperText: t['Date start']
+                      },
+                    }}
+                    onChange={handleStartDateChange}
+                    format="DD/MM/YYYY"
+                    components={{
+                      OpenPickerIcon: IconDate,
+                      CalendarIcon: IconDate,
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label={t.To}
+                    value={dayjs(endDate, 'DD/MM/YYYY')}
+                    slotProps={{
+                      textField: {
+                        // helperText: t['Date end']
+                      },
+                    }}
+                    onChange={handleEndDateChange}
+                    format="DD/MM/YYYY"
+                    components={{
+                      OpenPickerIcon: IconDate,
+                      CalendarIcon: IconDate,
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </div>
+
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="company-label">{t.Company}</InputLabel>
+                <Select labelId="company-label" value={selectedCompany} onChange={handleCompanyChange} className="delimite-text" IconComponent={IconArrow}>
+                  <MenuItem value="">
+                    <em>{t['All Companys']}</em>
+                  </MenuItem>
+                  {dataInitialSelect.oEmpresa?.map((comp) => (
+                    <MenuItem key={comp.id_empresa} value={comp.id_empresa}>
+                      <div> {comp.razon_social_empresa}</div>
+                    </MenuItem>
+                  ))}
+                </Select>
+                {/* <FormHelperText>{t['Select a company']}</FormHelperText> */}
+              </FormControl>
+
+             
+
+             
+
+            
+
+              <div className="box-clear">
+                <button className={`btn_primary small  ${hasAppliedFilters() ? '' : 'desactivo'}`} onClick={() => setApply(!apply)} disabled={!hasAppliedFilters()}>
+                  {t.Apply}
+                </button>
+
+                <button className={`btn_secundary small ${hasAppliedFilters() ? '' : 'desactivo'}`} onClick={handleClearFilters} disabled={!hasAppliedFilters()}>
+                  {t.Clear}
+                </button>
+              </div>
+            </div>
+
+
+
+
+
           <div className="tab-content">
             detractions
             <div className="tabOne">
